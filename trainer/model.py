276ca7bd96d7f6335a8e2a3e6e8f3c0227615dda
Christian Ramsey <thechristianramsey@gmail.com>
2018-01-02 16:46:50 -0800

    read_dataset() to return a callback function with the required signature
    We can now pass the result of read_dataset() to the Experiment class because the _input_fn has the required signature. Essentially, read_dataset() decorates the input function so that we can actually pass input parameters.

--- trainer/model.py
+++ trainer/model.py
@@ -21,20 +21,26 @@ DEFAULTS     = [[0.0],[0.0],[0.0],[0.0],[0.0],[0.0],\
 def read_dataset(filename, mode=tf.contrib.learn.ModeKeys.EVAL,
                  batch_size=512, num_training_epochs=10):
     num_epochs = num_training_epochs if mode == tf.contrib.learn.ModeKeys.TRAIN else 1
-    # could be a path to one file or a file pattern. (shuffling data means we lower our chance of leaving a batch for a slow worker
-    input_file_names = tf.train.match_filenames_once(filename)
-    filename_queue = tf.train.string_input_producer(
-        input_file_names, num_epochs=num_epochs, shuffle=True)
 
-    # read CSV from tf
-    reader = tf.TextLineReader()
-    _, value = reader.read_up_to(filename_queue, num_records=batch_size)
-    value_column = tf.expand_dims(value, -1)
-    columns = tf.decode_csv(value_column, record_defaults=DEFAULTS)
-    features = dict(zip(CSV_COLUMNS, columns))
-    label = features.pop(LABEL_COLUMN)
+    # the actual input function passed to TensorFlow
+    def _input_fn():
+        num_epochs = num_training_epochs if mode == tf.contrib.learn.ModeKeys.TRAIN else 1
 
-    return features, label
+        # could be a path to one file or a file pattern. (shuffling data means we lower our chance of leaving a batch for a slow worker
+        input_file_names = tf.train.match_filenames_once(filename)
+        filename_queue = tf.train.string_input_producer(
+            input_file_names, num_epochs=num_epochs, shuffle=True)
+
+        # read CSV
+        reader = tf.TextLineReader()
+        _, value = reader.read_up_to(filename_queue, num_records=batch_size)
+        value_column = tf.expand_dims(value, -1)
+        columns = tf.decode_csv(value_column, record_defaults=DEFAULTS)
+        features = dict(zip(CSV_COLUMNS, columns))
+        label = features.pop(LABEL_COLUMN)
+        return features, label
+
+    return _input_fn
 
 
 def get_features():
