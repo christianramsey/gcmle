8eda512264218174251357a984bc2e30ed7b8807
Christian Ramsey <thechristianramsey@gmail.com>
2018-01-03 15:57:09 -0800

    When sending jobs to ml engine be sure to send your extra args after --
    [-- USER_ARGS …]

--- scripts/distributed_training_job.sh
+++ scripts/distributed_training_job.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#!/bin/bash
+!/bin/bash
 
 PATTERN="Flights-00001*"
 
@@ -13,10 +13,10 @@ JOBNAME=flights_$(date -u +%y%m%d_%H%M%S)
 gcloud ml-engine jobs submit training $JOBNAME \
   --region=$REGION \
   --module-name=trainer.task \
-  --package-path=$(pwd)/trainer \
+  --package-path=$(pwd)../trainer \
   --job-dir=$OUTPUT_DIR \
   --staging-bucket=gs://$BUCKET \
   --scale-tier=STANDARD_1 \
    --output_dir=$OUTPUT_DIR \
-   --traindata $DATA_DIR/train$PATTERN --evaldata $DATA_DIR/test$PATTERN
-   
+   --traindata $DATA_DIR/train$PATTERN \
+   --evaldata $DATA_DIR/test$PATTERN
