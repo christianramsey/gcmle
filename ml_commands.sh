ca35b1b109e95c9594dc9f6cce1cbbe26b3cb21a
Christian Ramsey <thechristianramsey@gmail.com>
2018-01-03 14:50:50 -0800

    cleared out current dataset, ran model as python module/package instead of adhoc, worked successful (saved model here)

--- ml_commands.sh
+++ ml_commands.sh
@@ -8,3 +8,9 @@ python task.py \
        --traindata ../data/train.csv \
        --output_dir ./trained_model \
        --evaldata ../data/test.csv
+
+# run as python module
+export PYTHONPATH=${PYTHONPATH}:${PWD}/trainer
+python -m trainer.task \
+   --output_dir=./trained_model \
+  --traindata $DATA_DIR/train* --evaldata $DATA_DIR/test*
