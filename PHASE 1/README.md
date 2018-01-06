1817aa0926e7104f6d6efb7b1b1a0561a1fa9eb9
Christian Ramsey <thechristianramsey@gmail.com>
2018-01-06 10:17:30 -0800

    setup repository to have a phase 1, getting your model up and trained on the cloud

---
+++ PHASE 1/README.md
@@ -0,0 +1,33 @@
+# Distributed Deep Learning w/ Tensorflow 
+
+
+## Directory structure
+(img)
+- Directory Structure
+	- trainer [folder]
+		- task.py 
+			- this is where your main functions and command line args will be
+		- model.py 
+			- this is where your model will be
+		- \_\_init\_\_.py 
+			- this is required for your module
+	- data [folder]
+		- train.csv
+		- test.csv
+		- full\_dataset.csv
+	- utils.py
+	- setup.py
+		- this is where your module info will go (i.e. packages, version)
+
+
+## Read Dataset
+## Tensorflow Full Loop ML
+### Experiments
+Tensorflow has a built-in class that takes the full machine learning cycle and handles it in a distributed way if needed and asks that you 
+
+### Estimators
+### ExportStrategy
+
+
+## Read Dataset
+## Read Dataset
---
+++ PHASE 1/scripts/distributed_training_job.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+!/bin/bash
+
+# run locally
+python task.py \
+       --traindata ../data/train.csv \
+       --output_dir ../trained_model \
+       --evaldata ../data/test.csv
+
+
+
+
+PATTERN="Flights-00001*"
+
+
+BUCKET=flights_gcmle
+REGION=us-central1
+OUTPUT_DIR=gs://${BUCKET}/flights/chapter9/output
+DATA_DIR=gs://${BUCKET}/flights/chapter8/output
+JOBNAME=flights_$(date -u +%y%m%d_%H%M%S)
+gcloud ml-engine jobs submit training $JOBNAME \
+  --region=$REGION \
+  --module-name=trainer.task \
+  --package-path=$(pwd) \
+  --job-dir=$OUTPUT_DIR \
+  --staging-bucket=gs://$BUCKET \
+  --scale-tier=STANDARD_1 \
+  -- \
+   --output_dir=$OUTPUT_DIR \
+   --traindata $DATA_DIR/train$PATTERN \
+   --evaldata $DATA_DIR/test$PATTERN
+
+
+
+#   premium gpus
+BUCKET=flights_gcmle
+REGION=us-central1
+OUTPUT_DIR=gs://${BUCKET}/flights/chapter9/output
+DATA_DIR=gs://${BUCKET}/flights/chapter8/output
+JOBNAME=flights_$(date -u +%y%m%d_%H%M%S)
+gcloud ml-engine jobs submit training $JOBNAME \
+  --region=$REGION \
+  --module-name=trainer.task \
+  --package-path=$(pwd) \
+  --job-dir=$OUTPUT_DIR \
+  --staging-bucket=gs://$BUCKET \
+  --scale-tier=PREMIUM_1 \
+  -- \
+   --output_dir=$OUTPUT_DIR \
+   --traindata $DATA_DIR/train$PATTERN \
+   --evaldata $DATA_DIR/test$PATTERN
+
